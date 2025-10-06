c38a7a185cd24b3dd6830e15ab4a5b1d887dcb08
Milán Habányi <habanyim@gmail.com>
2025-10-06 13:12:27 +0200

    frontend modositas

---
+++ programozas/Music_analyzer/frontend/src/components/layout.js
@@ -0,0 +1,24 @@
+import { AppShell, Group, Button, Text, } from "@mantine/core";
+import { Link, Outlet } from "react-router-dom";
+import { IconHome, IconMusic } from "@tabler/icons-react";
+
+export default function Layout() {
+   return (
+     <AppShell header={{ height: 80 }} padding="40" style={{ background: '#f9f9f9' }}>
+       <AppShell.Header style={{ boxShadow: '0px 5px 10px 0px rgba(82, 63, 105, 0.05)', border: '1px solid #f1f1f1' }}>
+         <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", height: "100%" }}>
+           <Text fw={700} size="xl"> 🎧 Zene Alkalmazás </Text> 
+           <div style={{ flex: 1, display: "flex", justifyContent: "center" }}>
+              <Group> 
+                <Button component={Link} to="/" variant="filled" color="indigo" size="md" leftSection={<IconHome size={14} />}> Főoldal </Button>
+                <Button component={Link} to="/music-analyzer" variant="filled" color="indigo" size="md" leftSection={<IconMusic size={14} />}> Elemzés </Button> 
+              </Group> 
+            </div>
+          </div> 
+        </AppShell.Header>
+        <main style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "80vh" }}>
+           <Outlet /> 
+        </main> 
+      </AppShell>
+       ); 
+    }
--- programozas/Music_analyzer/frontend/src/index.js
+++ programozas/Music_analyzer/frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { MantineProvider } from '@mantine/core';
 import { BrowserRouter } from 'react-router-dom';
+import '@mantine/core/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
